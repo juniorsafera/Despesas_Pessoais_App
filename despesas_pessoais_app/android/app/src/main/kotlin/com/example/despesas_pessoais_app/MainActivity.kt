13771f37a0609fd810638f182d4e2ac63ea83710
Junior Safera <juniorsafera.dev@gmail.com>
2022-01-21 17:11:42 -0300

    criando projeto

---
+++ despesas_pessoais_app/android/app/src/main/kotlin/com/example/despesas_pessoais_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.despesas_pessoais_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ despesas_pessoais_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const DespesasApp());
+
+class DespesasApp extends StatelessWidget {
+  const DespesasApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: PaginaPrincipal(),
+    );
+  }
+}
+
+class PaginaPrincipal extends StatelessWidget {
+  const PaginaPrincipal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Início"),
+    );
+  }
+}
